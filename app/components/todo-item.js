968479ada1996899715f98304dad32c3d8fcc503
ceciliaTunapanda <cecilia@tunapanda.org>
2016-06-25 10:47:58 +0300

    Delete items on todo list

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'li', 
+	actions: {
+		toggleComplete () {
+			this.toggleProperty('isCompleted');
+		},
+	} 
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,9 +4,13 @@ export default Ember.Controller.extend({
 	todos: [],
 
 	actions: {
-				createTodo(content){
-			this.store.createRecord('todo', {label: content});
+		createTodo(content){
 			this.get('todos').addObject(this.store.createRecord('todo', {label: content}));
+		},
+		destroyTodo(todo){
+			this.get('todos').removeObject(todo);	
+			todo.deleteRecord();
 		}
+
 	}
 });
